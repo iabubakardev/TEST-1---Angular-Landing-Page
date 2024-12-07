54bd95659a3474878d8cb53348c8f0223e098606
Abu Bakar <iabubakardev@gmail.com>
2024-12-07 10:51:25 +0500

    TEST 1 - Angular Landing Page

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { MainComponent } from '../../components/main/main.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeaderComponent,MainComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
